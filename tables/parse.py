45c9b08ed0749e3cae4497c399db272e29d85e34
Blake Xiao Long <ajimenez3600@gmail.com>
2017-04-14 22:54:23 -0400

    tables

---
+++ tables/parse.py
@@ -0,0 +1,14 @@
+of = open("slotless.txt","w")
+with open("slotless.csv","r") as fp:
+    for line in fp:
+        if line != "\n":
+            a,x,y = line.split("?")
+            if len(a) > 3:
+                a=a.replace("-",",")
+                print("atlas.push_back(Item(" + a + r',"' + x + r'"' + "));", file=of)
+            else:
+                print("atlas.push_back(Item(" + a + "," + a + "," + x + "));", file=of)
+        else:
+            print("",file=of)
+
+fp.close()
