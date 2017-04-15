2ee11393c1d18ec763a4ced5a3b8f2c0d31df0f4
Blake Xiao Long <ajimenez3600@gmail.com>
2017-04-14 23:52:35 -0400

    Signatures for generation functions

--- TreasureGen-Mk2/Generator.cpp
+++ TreasureGen-Mk2/Generator.cpp
@@ -10,3 +10,52 @@ int main()
 	vector<vector<LevelledItem>> wands, scrolls, potions;
 	buildEverything(armor, weapons, art, gems, wands, scrolls, potions, staffs, rods, rings, wondrous);
 }
+
+stringstream coinGen(int num, int die, Coin = gp, int multiplier = 1)
+{
+
+}
+stringstream gemGen(int grade, int num = 1)
+{
+
+}
+stringstream artGen(int grade, int num = 1)
+{
+
+}
+stringstream scrollGen(Grade grade, int num = 1)
+{
+
+}
+stringstream wandGen(Grade grade, int num = 1)
+{
+
+}
+stringstream potionGen(Grade grade, int num = 1)
+{
+
+}
+stringstream armorGen(Grade grade, int num = 1)
+{
+
+}
+stringstream weaponGen(Grade grade, int num = 1)
+{
+
+}
+stringstream ringGen(Grade grade, int num = 1)
+{
+
+}
+stringstream wondrousGen(Grade grade, int num = 1)
+{
+
+}
+stringstream staffGen(Grade grade, int num = 1)
+{
+
+}
+stringstream rodGen(Grade grade, int num = 1)
+{
+
+}
--- TreasureGen-Mk2/Generator.h
+++ TreasureGen-Mk2/Generator.h
@@ -11,3 +11,18 @@
 #include "Weapon.h"
 #include "Armor.h"
 #include "Atlas.h"
+
+enum Coin { cp, sp, gp, pp };
+enum Grade { LMi, GMi, LMe, GMe, LMa, GMa };
+stringstream coinGen(int num, int die, Coin = gp, int multiplier = 1);
+stringstream gemGen(int grade, int num = 1);
+stringstream artGen(int grade, int num = 1);
+stringstream scrollGen(Grade grade, int num = 1);
+stringstream wandGen(Grade grade, int num = 1);
+stringstream potionGen(Grade grade, int num = 1);
+stringstream armorGen(Grade grade, int num = 1); // grade = 0 for masterwork
+stringstream weaponGen(Grade grade, int num = 1); // ^
+stringstream ringGen(Grade grade, int num = 1);
+stringstream wondrousGen(Grade grade, int num = 1);
+stringstream staffGen(Grade grade, int num = 1);
+stringstream rodGen(Grade grade, int num = 1);
